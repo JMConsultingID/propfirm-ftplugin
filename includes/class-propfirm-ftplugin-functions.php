fd9393df491a9d5e89b69e2910a0f69c0a011001
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2023-09-06 10:38:16 +0800

    Update class-propfirm-ftplugin-functions.php

--- includes/class-propfirm-ftplugin-functions.php
+++ includes/class-propfirm-ftplugin-functions.php
@@ -56,8 +56,8 @@ function propfirm_ftplugin_settings_init() {
     add_settings_field('ft_enable_plugin', 'Enable Plugin', 'ft_enable_plugin_callback', 'propfirm-ftplugin', 'propfirm_ftplugin_general_section');
     add_settings_field('ft_select_cpt', 'Select Custom Post Type', 'ft_select_cpt_callback', 'propfirm-ftplugin', 'propfirm_ftplugin_general_section');
     add_settings_field('ft_select_taxonomy', 'Select Taxonomy', 'ft_select_taxonomy_callback', 'propfirm-ftplugin', 'propfirm_ftplugin_general_section');
-    add_settings_field('ft_archive_enable', 'Redirect old URL', 'ft_post_type_archive_callback', 'propfirm-ftplugin', 'propfirm_ftplugin_general_section');
-    add_settings_field('ft_select_redirect_old_url', 'Post Type Archive', 'ft_select_redirect_callback', 'propfirm-ftplugin', 'propfirm_ftplugin_general_section');
+    add_settings_field('ft_archive_enable', 'Post Type Archive', 'ft_post_type_archive_callback', 'propfirm-ftplugin', 'propfirm_ftplugin_general_section');
+    add_settings_field('ft_select_redirect_old_url', 'Redirect old URL', 'ft_select_redirect_callback', 'propfirm-ftplugin', 'propfirm_ftplugin_general_section');
     add_settings_field('ft_flush_rewrite', 'Flush Rewrite Rules', 'ft_flush_rewrite_rules_callback', 'propfirm-ftplugin', 'propfirm_ftplugin_general_section');
     //add_settings_field('ft_reset_settings', 'Reset Plugin Settings', 'ft_reset_settings_callback', 'propfirm-ftplugin', 'propfirm_ftplugin_general_section');
 }
