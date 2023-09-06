43df0299f8c4849109e1529983d94d697aeb6190
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2023-09-06 10:15:56 +0800

    restructure functions permalink

--- includes/class-propfirm-ftplugin-functions-permalink.php
+++ includes/class-propfirm-ftplugin-functions-permalink.php
@@ -15,118 +15,103 @@ if (is_propfirm_ftplugin_enabled()) {
     if (!ft_required_options_set()) {
         return; // Jika custom post type atau custom taxonomy belum diatur, keluar dari fungsi
     }
-
     // Mengubah link post
     add_filter('post_type_link', 'ft_custom_permalink_structure', 10, 2);
-    function ft_custom_permalink_structure($post_link, $post) {
-        $options = get_option('propfirm_ftplugin_settings');
-        if (isset($options['select_cpt']) && $post->post_type == $options['select_cpt']) {
-            $terms = get_the_terms($post->ID, $options['select_taxonomy']);
-            if ($terms) {
-                return home_url($terms[0]->slug . '/' . $post->post_name . '/');
-            }
-        }
-        return $post_link;
-    }
-
-
     // Menambahkan rewrite rules
     add_action('init', 'ft_add_rewrite_rules');
-    function ft_add_rewrite_rules() {
-        $options = get_option('propfirm_ftplugin_settings');
-        global $post;
-        // Jika ini adalah halaman atau post biasa, keluar dari fungsi
-        if (is_page() || is_singular('post')) {
-            return;
-        }
-        if (isset($options['select_cpt'])) {
-            $casino_categories = get_terms(array('taxonomy' => $options['select_taxonomy'], 'hide_empty' => 0));
-            foreach ($casino_categories as $category) {
-                add_rewrite_rule('^' . $category->slug . '/([^/]+)/?$', 'index.php?post_type=' . $options['select_cpt'] . '&name=$matches[1]', 'top');
-            }
-        }
-    }
-
-
     // Mengubah link kategori
     add_filter('term_link', 'ft_custom_category_permalink', 10, 3);
-    function ft_custom_category_permalink($url, $term, $taxonomy) {
-        $options = get_option('propfirm_ftplugin_settings');
-        if ($taxonomy == $options['select_taxonomy'] && isset($options['select_cpt'])) {
-            return home_url($term->slug . '/');
-        }
-        return $url;
-    }
-
-
     // Menambahkan rewrite rules untuk kategori
     add_action('init', 'ft_add_category_rewrite_rules');
-    function ft_add_category_rewrite_rules() {
-        $options = get_option('propfirm_ftplugin_settings');
-
-        global $post;
-        // Jika ini adalah halaman atau post biasa, keluar dari fungsi
-        if (is_page() || is_singular('post')) {
-            return;
-        }
-
-        if (isset($options['select_cpt'])) {
-            $casino_categories = get_terms(array('taxonomy' => $options['select_taxonomy'], 'hide_empty' => 0));
-            foreach ($casino_categories as $category) {
-                add_rewrite_rule('^' . $category->slug . '/?$', 'index.php?casino-category=' . $category->slug, 'top');
-            }
-        }
-    }
-
+    // Menambahkan fungsi redirect old permalink to new permalink
     add_action('template_redirect', 'ft_redirect_old_cpt_urls_to_new');
-    function ft_redirect_old_cpt_urls_to_new() {
-        $options = get_option('propfirm_ftplugin_settings');
+    // Menambahkan fungsi request_filter
+    add_filter('request', 'ft_custom_request_filter');    
+}
 
-        // Memeriksa apakah select_redirect diaktifkan
-        if (!isset($options['select_redirect']) || $options['select_redirect'] !== 'enable') {
-            return; // Jika tidak diaktifkan, keluar dari fungsi
+function ft_custom_permalink_structure($post_link, $post) {
+    $options = get_option('propfirm_ftplugin_settings');
+    if (isset($options['select_cpt']) && $post->post_type == $options['select_cpt']) {
+        $terms = get_the_terms($post->ID, $options['select_taxonomy']);
+        if ($terms) {
+            return home_url($terms[0]->slug . '/' . $post->post_name . '/');
         }
+    }
+    return $post_link;
+}
 
-        global $post;
-
-        // Jika ini adalah halaman atau post biasa, keluar dari fungsi
-        if (is_page() || is_singular('post')) {
-            return;
+function ft_add_rewrite_rules() {
+    $options = get_option('propfirm_ftplugin_settings');
+    global $post;
+    // Jika ini adalah halaman atau post biasa, keluar dari fungsi
+    if (is_page() || is_singular('post')) {
+        return;
+    }
+    if (isset($options['select_cpt'])) {
+        $casino_categories = get_terms(array('taxonomy' => $options['select_taxonomy'], 'hide_empty' => 0));
+        foreach ($casino_categories as $category) {
+            add_rewrite_rule('^' . $category->slug . '/([^/]+)/?$', 'index.php?post_type=' . $options['select_cpt'] . '&name=$matches[1]', 'top');
         }
+    }
+}
 
-        if (isset($options['select_cpt']) && is_single() && $post->post_type == $options['select_cpt']) {
-            $casino_categories = get_the_terms($post->ID, $options['select_taxonomy']);
-            if ($casino_categories && !is_wp_error($casino_categories)) {
-                $category_slug = $casino_categories[0]->slug;
-                $expected_url = home_url("/{$category_slug}/{$post->post_name}/");
-                if ($_SERVER['REQUEST_URI'] != parse_url($expected_url, PHP_URL_PATH)) {
-                    wp_redirect($expected_url, 301);
-                    exit;
-                }
+function ft_custom_category_permalink($url, $term, $taxonomy) {
+    $options = get_option('propfirm_ftplugin_settings');
+    if ($taxonomy == $options['select_taxonomy'] && isset($options['select_cpt'])) {
+        return home_url($term->slug . '/');
+    }
+    return $url;
+}
+
+function ft_add_category_rewrite_rules() {
+    $options = get_option('propfirm_ftplugin_settings');
+
+    global $post;
+    // Jika ini adalah halaman atau post biasa, keluar dari fungsi
+    if (is_page() || is_singular('post')) {
+        return;
+    }
+
+    if (isset($options['select_cpt'])) {
+        $casino_categories = get_terms(array('taxonomy' => $options['select_taxonomy'], 'hide_empty' => 0));
+        foreach ($casino_categories as $category) {
+            add_rewrite_rule('^' . $category->slug . '/?$', 'index.php?casino-category=' . $category->slug, 'top');
+        }
+    }
+}
+
+function ft_redirect_old_cpt_urls_to_new() {
+    $options = get_option('propfirm_ftplugin_settings');
+
+    // Memeriksa apakah select_redirect diaktifkan
+    if (!isset($options['select_redirect']) || $options['select_redirect'] !== 'enable') {
+        return; // Jika tidak diaktifkan, keluar dari fungsi
+    }
+
+    global $post;
+
+    // Jika ini adalah halaman atau post biasa, keluar dari fungsi
+    if (is_page() || is_singular('post')) {
+        return;
+    }
+
+    if (isset($options['select_cpt']) && is_single() && $post->post_type == $options['select_cpt']) {
+        $casino_categories = get_the_terms($post->ID, $options['select_taxonomy']);
+        if ($casino_categories && !is_wp_error($casino_categories)) {
+            $category_slug = $casino_categories[0]->slug;
+            $expected_url = home_url("/{$category_slug}/{$post->post_name}/");
+            if ($_SERVER['REQUEST_URI'] != parse_url($expected_url, PHP_URL_PATH)) {
+                wp_redirect($expected_url, 301);
+                exit;
             }
         }
     }
+}
 
-    add_filter('request', 'ft_custom_request_filter');
-    function ft_custom_request_filter($query_vars) {
-        $options = get_option('propfirm_ftplugin_settings');
-        if (isset($query_vars[$options['select_taxonomy']]) && isset($options['select_cpt'])) {
-            $query_vars['post_type'] = $options['select_cpt'];
-        }
-        return $query_vars;
+function ft_custom_request_filter($query_vars) {
+    $options = get_option('propfirm_ftplugin_settings');
+    if (isset($query_vars[$options['select_taxonomy']]) && isset($options['select_cpt'])) {
+        $query_vars['post_type'] = $options['select_cpt'];
     }
-
-}
-
-// Flush rewrite rules saat plugin diaktifkan
-register_activation_hook(__FILE__, 'ft_flush_rewrite_rules');
-function ft_flush_rewrite_rules() {
-    ft_add_rewrite_rules();
-    flush_rewrite_rules();
-}
-
-// Flush rewrite rules saat plugin dinonaktifkan
-register_deactivation_hook(__FILE__, 'ft_flush_rewrite_rules_deactivate');
-function ft_flush_rewrite_rules_deactivate() {
-    flush_rewrite_rules();
+    return $query_vars;
 }
--- includes/class-propfirm-ftplugin-functions.php
+++ includes/class-propfirm-ftplugin-functions.php
@@ -58,7 +58,7 @@ function propfirm_ftplugin_settings_init() {
     add_settings_field('ft_select_taxonomy', 'Select Taxonomy', 'ft_select_taxonomy_callback', 'propfirm-ftplugin', 'propfirm_ftplugin_general_section');
     add_settings_field('ft_select_redirect_old_url', 'Redirect old URL', 'ft_select_redirect_callback', 'propfirm-ftplugin', 'propfirm_ftplugin_general_section');
     add_settings_field('ft_flush_rewrite', 'Flush Rewrite Rules', 'ft_flush_rewrite_rules_callback', 'propfirm-ftplugin', 'propfirm_ftplugin_general_section');
-    add_settings_field('ft_reset_settings', 'Reset Plugin Settings', 'ft_reset_settings_callback', 'propfirm-ftplugin', 'propfirm_ftplugin_general_section');
+    //add_settings_field('ft_reset_settings', 'Reset Plugin Settings', 'ft_reset_settings_callback', 'propfirm-ftplugin', 'propfirm_ftplugin_general_section');
 }
 
 function ft_enable_plugin_callback() {
@@ -133,4 +133,17 @@ function ft_handle_reset_settings() {
 
 function ft_settings_reset_notice() {
     echo '<div class="updated"><p>Plugin settings have been reset.</p></div>';
+}
+
+// Flush rewrite rules saat plugin diaktifkan
+register_activation_hook(__FILE__, 'ft_flush_rewrite_rules');
+function ft_flush_rewrite_rules() {
+    ft_add_rewrite_rules();
+    flush_rewrite_rules();
+}
+
+// Flush rewrite rules saat plugin dinonaktifkan
+register_deactivation_hook(__FILE__, 'ft_flush_rewrite_rules_deactivate');
+function ft_flush_rewrite_rules_deactivate() {
+    flush_rewrite_rules();
 }
